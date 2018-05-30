a86fe1b2719713bf0cb88b8ed3d68f5e6c19e854
lbugnion <lbugnion@microsoft.com>
2018-05-30 11:51:50 +0200

    Updated HelloXamarin with .NET Standard

--- HelloXamarin MVVM/HelloIos/Main.cs
+++ HelloXamarin MVVM/HelloIos/Main.cs
@@ -1,21 +1,30 @@
-﻿using HelloAndroid.ViewModel;
+﻿using Data.ViewModel;
+using System;
 using UIKit;
 
 namespace HelloIos
 {
-    public class Application
-    {
+	public class Application
+	{
         private static ViewModelLocator _locator;
 
         public static ViewModelLocator Locator 
             => _locator ?? (_locator = new ViewModelLocator());
 
-        // This is the main entry point of the application.
-        static void Main(string[] args)
-        {
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            try
+            {
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch (Exception ex)
+            {
+
+            }
+
         }
-    }
+	}
 }
